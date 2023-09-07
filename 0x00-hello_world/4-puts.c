ccd9838bfadbf7131aa4f8aac9f4753dd8220eaa
DEAAroot <alshapshere@gmail.com>
2023-09-07 14:24:34 +0000

    winningtime

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,10 @@
 /**
  *main - entry point
  *
- * Description: A C program that prints puts function*/
-int main (void)
+ * Description: A C program that prints puts functiodn
+ * Return: Always 0 (Success)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
