70b2ae86afce348be95d127bbfe37269f63797a9
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-10-25 04:15:35 +0300

    w

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,19 +2,19 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * @i: linked list of type listint_t to traverse
  *
  * Return: number of nodes
  */
-size_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t *i)
 {
-	size_t num = 0;
+	size_t number = 0;
 
-	while (h)
+	while (i)
 	{
-		num++;
-		h = h->next;
+		number++;
+		i = i->next;
 	}
 
-	return (num);
+	return (number);
 }
