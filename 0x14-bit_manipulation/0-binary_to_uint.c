fcdf24ffab1be6011029db2a424141eadbede1fb
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-10-26 17:36:23 +0300

    dd

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,34 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int ui;  // Initialize an unsigned integer variable to store the result.
-    int len, base_two;  // Declare integer variables for the length and a base factor.
+	unsigned int ui;
+	int len, base_two;
 
-    if (!b)  // Check if the input binary string is NULL.
-        return (0);  // Return 0 if the input is NULL.
+	if (!b)
+		return (0);
 
-    ui = 0;  // Initialize the result to 0.
+	ui = 0;
 
-    // Calculate the length of the binary string by counting characters.
-    for (len = 0; b[len] != '\0'; len++)
-        ;
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-    // For the binary representation, you'll need a base factor that starts at 1.
-    // This factor will be used to calculate the decimal equivalent.
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-    // Now, let's process the binary string from right to left.
-    for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-    {
-        if (b[len] != '0' && b[len] != '1')
-        {
-            return (0);  // If the character is not '0' or '1, return 0 (error).
-        }
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
 
-        if (b[len] & 1)
-        {
-            ui += base_two;  // If the character is '1', add the corresponding power of 2.
-        }
-    }
-
-    return (ui);  // Return the calculated unsigned integer.
+	return (ui);
 }
