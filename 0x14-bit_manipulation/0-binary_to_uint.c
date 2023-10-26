445fdacfc271742c50fdec5d8bb157dbfa497b20
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-10-26 17:11:23 +0300

    sd

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,22 @@
  */
 unsigned int binary_to_uint(const char *b) {
     if (b == NULL) {
-        return 0; // Return 0 if the input string is NULL.
+        return 0;
     }
 
     unsigned int result = 0;
     int i = 0;
 
     while (b[i] != '\0') {
-        // Check if the character is '0' or '1'
+       
         if (b[i] == '0') {
-            // Shift the current result left by 1 bit and add 0
+            
             result = (result << 1);
         } else if (b[i] == '1') {
-            // Shift the current result left by 1 bit and add 1
+         
             result = (result << 1) | 1;
         } else {
-            return 0; // Return 0 if a character is not '0' or '1'.
+            return 0;
         }
         i++;
     }
