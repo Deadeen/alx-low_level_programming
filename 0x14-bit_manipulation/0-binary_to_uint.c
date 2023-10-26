bf4fe5af09bcd15d3b148f5192d20eea68af2d02
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-10-26 17:12:08 +0300

    ss

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,28 +7,31 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *b) {
-    if (b == NULL) {
-        return 0;
-    }
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int len, base_two;
 
-    unsigned int result = 0;
-    int i = 0;
+	if (!b)
+		return (0);
 
-    while (b[i] != '\0') {
-       
-        if (b[i] == '0') {
-            
-            result = (result << 1);
-        } else if (b[i] == '1') {
-         
-            result = (result << 1) | 1;
-        } else {
-            return 0;
-        }
-        i++;
-    }
+	ui = 0;
 
-    return result;
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
 }
-
